93c13d6314eac1fefda498bd3dedbdc64adb6511
afrobintalentum <desarrollador.web01@talentum.edu.co>
2024-04-11 16:50:33 -0500

    Update block_eco_voice.php

--- block_eco_voice.php
+++ block_eco_voice.php
@@ -1,12 +1,26 @@
 <?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
-/*
- * Block definition class for the block_pluginname plugin.
+/**
+ * Plugin version and other meta-data are defined here.
  *
- * @package   block_eco_voice
- * @copyright 2024, Mateo Olaya <desarrollador.web03@talentum.edu.co>
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
-*/
+ * @package     local_greetings
+ * @copyright   2024, Mateo Olaya <desarrollador.web03@talentum.edu.co>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 //* Creando clase para el bloque ecoVoice
 class block_eco_voice extends block_base {
